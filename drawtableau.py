9851119eef6a3c7f9d1c2d50d10febabff5736df
Renata Paramastri <rparamastri@cs.hmc.edu>
2016-11-18 19:55:13 -0800

    Wrote CLI

---
+++ drawtableau.py
@@ -0,0 +1,31 @@
+import argparse
+import semantics, parser, tree_structure
+
+def main():
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument("filename", help="the file containing your code")
+
+    argparser.add_argument("-f", "--format", help="format of the image file "
+                                               "('pdf', 'png', etc.)",
+                           default='png')
+    argparser.add_argument("-o", "--out", help="the name of the image file",
+                           default='out')
+    argparser.add_argument("-d", "--dot", help="produce the DOT code as well",
+                           action="store_true")
+    
+    args = argparser.parse_args()
+
+    f = open(args.filename, 'r') 
+    user_code = f.read()
+    f.close()
+
+    node_list = parser.parser.parse(user_code)
+    tree      = tree_structure.Tree(node_list)
+    
+    semantics.draw(tree, args.format, args.out, args.dot)
+    print("Drew to {}.{}".format(args.out, args.format))
+
+if __name__ == '__main__':
+    main()
+
