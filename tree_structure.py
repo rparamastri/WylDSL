7963ce60bacd97a73728a8ed21d5c343c67d07ee
Renata Paramastri <rparamastri@cs.hmc.edu>
2016-11-28 13:33:44 -0800

    Restructured Symbol class
    Simplified structure, eliminating unnecessary member variables and
    functions.
    Makes line numbers for key errors possible.

--- tree_structure.py
+++ tree_structure.py
@@ -69,21 +69,3 @@ class Symbol:
             'check':'✓'
             }
 
-    def __init__(self, keyword):
-        self.keyword = keyword
-
-    def get_symbol(self):
-        try:
-            return self.symbol_dict[self.keyword]
-        except KeyError as e:  
-            print('The symbol keyword "{}" is not defined.\n'
-                  'Define symbol keywords by writing:\n'
-                  '\tkeyword $<keyword> for <symbol>\n'
-                  'where <symbol> is the literal string\n'
-                  'or HTML decimal values'.format(self.keyword)
-                 )
-            raise
-
-    def __repr__(self):
-        return "Symbol({})".format(self.keyword)
-
