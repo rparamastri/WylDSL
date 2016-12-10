f747d7ff9d15db9d26c338d1fc544a7199e8caf7
Renata Paramastri <rparamastri@cs.hmc.edu>
2016-12-09 21:33:34 -0800

    explained computation of edges

--- tree_structure.py
+++ tree_structure.py
@@ -23,6 +23,11 @@ class Tree:
         self.find_parent(found_child)
 
     def find_first_child(self):
+        """
+        Computes edges between parent nodes and their first child.
+        Returns a list of indices of parents (nodes that are not leaves).
+        """
+        # no need to examine the last two nodes, as they cannot be parents
         found_child = [i for i,node in enumerate(self.nodes[:-2]) 
                 if node.depth+1 == self.nodes[i+1].depth
                 ]
@@ -34,11 +39,18 @@ class Tree:
         return found_child
 
     def find_parent(self,found_child):
+        """
+        Given indices of parent (non-leaf) nodes, find their second child.
+        """
         for i in found_child:
+            # the node at i is itself and the node at i+1 is the first child
+            # the second child is somewhere after those two
             for node in self.nodes[i+2:]:
+                # the first node encountered with the right depth 
                 if node.depth == self.nodes[i].depth + 1:
                     self.edges.append((self.nodes[i].get_id(), node.get_id()))
                     break
+                # find a sibling node before the second child
                 elif node.depth == self.nodes[i].depth:
                     print("Error! Cannot construct tree.")
                     raise Exception
