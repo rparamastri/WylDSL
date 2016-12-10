9ca6201282a73adef2ec66f27e12e82ca7a83628
Renata Paramastri <rparamastri@cs.hmc.edu>
2016-12-09 21:26:23 -0800

    add documentation

--- drawtableau.py
+++ drawtableau.py
@@ -2,6 +2,9 @@ import argparse
 import semantics, parser, tree_structure
 
 def main():
+    """
+    Takes in arguments and calls the draw function.
+    """
     argparser = argparse.ArgumentParser()
 
     argparser.add_argument("filename", help="the file containing your code")
@@ -16,10 +19,12 @@ def main():
     
     args = argparser.parse_args()
 
+    # read the file containing the code
     f = open(args.filename, 'r') 
     user_code = f.read()
     f.close()
 
+    # get the structure of the tree
     node_list = parser.parser.parse(user_code)
     tree      = tree_structure.Tree(node_list)
     
