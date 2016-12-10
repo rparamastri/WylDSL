7b51acd091295e76c98ef18bbfaf49c2771be200
Renata Paramastri <rparamastri@cs.hmc.edu>
2016-12-09 21:34:11 -0800

    add documentation

--- semantics.py
+++ semantics.py
@@ -5,9 +5,11 @@ from graphviz import Graph
 def draw(tree, f_format, fname, dot_file):
     g = Graph(format=f_format, node_attr={'shape':'plaintext'})
 
+    # add nodes to the graph
     for tree_node in tree.nodes:
         g.node(tree_node.get_id(), '<'+tree_node.text+'>')
 
+    # add edges to the graph
     for tree_edge in tree.edges:
         g.edge(*tree_edge)
 
